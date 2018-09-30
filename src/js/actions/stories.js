3953ee143d617a8c443b4a486bd3bf1bae2339c8
Bartosz Lewandowski <barteklewandowski94@gmail.com>
2018-10-01 01:01:31 +0200

    changed url

--- src/js/actions/stories.js
+++ src/js/actions/stories.js
@@ -21,11 +21,11 @@ const requestListFailure = () => ({
 export const getList = filters => (dispatch) => {
   dispatch(requestList(filters));
 
-  return axios.get(API_URLS.NEW_STORIES)
+  return axios.get(API_URLS.TOP_STORIES)
     .then((result) => {
       const { data } = result;
 
-      dispatch(requestListSuccess(data.sort((a, b) => b - a), data.length));
+      dispatch(requestListSuccess(data, data.length));
     })
     .catch(err => dispatch(requestListFailure(err)));
 };
